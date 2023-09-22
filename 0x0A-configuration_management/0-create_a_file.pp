afa8b71d94d6d0120b2a39c10f45427f7256e640
henry offor <henryoffor007@gmail.com>
2023-09-22 12:40:29 -0700

    task 0 update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# a file in /tmp
-
+# a file in /tmp.
 file { '/tmp/school':
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
